9dbbd7812f5ec4d54d70900fa90121750108ba6d
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2025-03-29 15:40:24 -0400

    Initial Commit

---
+++ Shifa/ShifaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShifaApp.swift
+//  Shifa
+//
+//  Created by Fahim Uddin on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ShifaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
